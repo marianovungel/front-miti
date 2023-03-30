29f8bcc8b6dd9f5c2ee5b1b944a4771b3a27d52a
marianovungel <vungem88@gmail.com>
2023-03-29 23:06:33 -0300

    segunda

--- src/pages/Cadastrar/Cadastrar.jsx
+++ src/pages/Cadastrar/Cadastrar.jsx
@@ -1,6 +1,5 @@
 import axios from 'axios'
 import React, { useState } from 'react'
-import { useCookies } from 'react-cookie'
 import { Link, useNavigate } from 'react-router-dom'
 import './style.css'
 
