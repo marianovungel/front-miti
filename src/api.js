e14e69db8e1d3ca4aab28156b280b64538ac5570
marianovungel <vungem88@gmail.com>
2023-04-01 19:48:25 -0300

    array

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-    baseURL: "http://localhost:8000/"
-    // baseURL: "https://backmiti.onrender.com/"
+    // baseURL: "http://localhost:8000/"
+    baseURL: "https://backmiti.onrender.com/"
 });
 export default api;
--- src/components/jogoEscolha/JogoEscolha.jsx
+++ src/components/jogoEscolha/JogoEscolha.jsx
@@ -19,7 +19,7 @@ export default function JogoEscolha({
 
     useEffect(()=>{
         setQuestion(data[questionNumber-1]);
-        if(questionNumber===20){
+        if(questionNumber===27){
             setFim(false)
         }
     }, [data, questionNumber])
